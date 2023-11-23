3bf67389bc752e818b346a047a67eedfe5fb2fbc
YES BPO <129446926+yesbpo@users.noreply.github.com>
2023-11-22 21:08:24 -0500

    Update templates.js

--- pages/templates.js
+++ pages/templates.js
@@ -10,7 +10,11 @@ const Reports = (props) => {
   const [templateExample, setTemplateExample] = useState('');
   const [templateName, setTemplateName] = useState('');
   const [templateCategory, setTemplateCategory] = useState('');
+  const [templateType, setTemplateType] = useState('');
+  const [showTemplateOptions, setShowTemplateOptions] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('none');
 
+//Crear plantilla
   const apiUrl = 'https://api.gupshup.io/wa/app/cef6cd40-330f-4b25-8ff2-9c8fcc434d90/template';
   const apiKey = '6ovjpik6ouhlyoalchzu4r2csmeqwlbg';
 
@@ -74,31 +78,113 @@ const Reports = (props) => {
     fetchData(); // Llama a la función para obtener datos al montar el componente
   }, []); // El segundo argumento [] significa que se ejecutará solo al montar y desmontar el componente
 
+//Tipo de plantilla
+  const handleTemplateTypeChange = (event) => {
+    setTemplateType(event.target.value);
+    setShowTemplateOptions(event.target.value === 'product');
+  };
+  
+  const handleInputChange = (e) => {
+    const inputValue = e.target.value;
+
+    // Limitar la longitud del texto a 1024 caracteres
+    if (inputValue.length <= 1024) {
+      setTemplateBody(inputValue);
+    }
+  };
+
+  const charactersRemaining = 1024 - templateBody.length;
+
+  const handleSelectChange = (e) => {
+    setSelectedOption(e.target.value);
+    setTemplateCategory(e.target.value);
+  };
+
+  let categoryRequirementText = null;
+
+  if (templateCategory === 'MARKETING') {
+    categoryRequirementText = 'Envíe ofertas, promociones de productos y otras catalogos para aumentar tu eficacia y productividad.';
+  } else if (templateCategory === 'UTILITY') {
+    categoryRequirementText = 'Envíe actualizaciones de cuentas, pedidos, recordatorios y mucho más. Podras enviar información importante .';
+  } else if (templateCategory === 'AUTHEM') {
+    categoryRequirementText = 'Envíe codigos que autoricen accesos o de confirmación.';
+  }
 
   return (
     <Layout>
 
-      <Box>
+<Box>
       <form onSubmit={handleSubmit}>
-        <div>
+        <InputContainer>
           <label>
             Nombre plantilla: * 
-            <input type="text" value={templateName} onChange={(e) => setTemplateName(e.target.value)} /> 
+            <Input
+              type="text"
+              value={templateName}
+              onChange={(e) => setTemplateName(e.target.value)}
+            />
           </label>
-        </div>
+          <RequirementText>
+            Requisitos: Todo en minúsculas, espacios separados por barras al piso (_), sin caracteres especiales.
+          </RequirementText>
+        </InputContainer>
 
         <div>
       <label>
         Categoria: * 
-        <select 
+        <SelectInput 
           value={templateCategory} 
-          onChange={(e) => setTemplateCategory(e.target.value)}
+          onChange={handleSelectChange}
         >
           <option value="MARKETING">Marketing</option>
           <option value="UTILITY">Utilidad</option>
           <option value="AUTHEM">Autenticación</option>
-        </select>
+        </SelectInput>
       </label>
+      {categoryRequirementText && (
+        <RequirementText>
+          {categoryRequirementText}
+        </RequirementText>
+      )}
+    </div>
+
+    <div>
+      <div>
+        <label>
+          <input
+            type='radio'
+            name='templateType'
+            value='custom'
+            onChange={handleTemplateTypeChange}
+          />
+          Plantilla personalizada
+        </label>
+
+        <label>
+          <input
+            type='radio'
+            name='templateType'
+            value='product'
+            onChange={handleTemplateTypeChange}
+          />
+          Plantilla de producto
+        </label>
+      </div>
+
+      {showTemplateOptions && (
+        <div>
+          <p>Formato de plantilla: </p>
+          <label>
+            <input type='radio' name='templateFormat' />
+            Plantilla catálogo
+          </label>
+
+          <label>
+            <input type='radio' name='templateFormat' />
+            Plantilla multiproducto
+          </label>
+        </div>
+      )}
     </div>
 
     <div>
@@ -112,24 +198,45 @@ const Reports = (props) => {
     <option value="es_ARG">Español Argentina</option>
     <option value="es_ES">Español España</option>
     <option value="en_US">Ingles Estados Unidos</option>
-    {/* Agrega otras opciones de idioma si es necesario */}
   </select>
 </label>
 </div>
       
-      <div>
-        <label>
-          Texto:  * 
-          <textarea value={templateBody} onChange={(e) => setTemplateBody(e.target.value)} />
-        </label>
-      </div>
+<div>
+      <label>
+        Texto:  * 
+        <StyledTextArea 
+          value={templateBody} 
+          onChange={handleInputChange} 
+          rows="4"
+          maxLength={1024} 
+        />
+      </label>
+      <RequirementText>
+        Caracteres restantes: {charactersRemaining}
+      </RequirementText>
+    </div>
 
       <div>
         <label>
         Pie de pagina:
         <input type="text" value={templateFooter} onChange={(e) => setTemplateFooter(e.target.value)} />
         </label>
+        <RequirementText>
+          Opcional
+        </RequirementText>
       </div>
+
+      <div>
+      <label>
+        Tipo de acción:
+        <select value={selectedOption} onChange={handleSelectChange}>
+          <option value="none">Ninguno</option>
+          <option value="callToAction">Llamada a la acción</option>
+          <option value="quickReply">Respuesta rápida</option>
+        </select>
+      </label>
+    </div>
 
       <div>
       <label>
@@ -201,6 +308,39 @@ const StyledTemplateBox = styled.div`
   border-radius: 8px;
   background-color: #f8f8f8;
   font-size: 16px;
+`;
+
+const InputContainer = styled.div`
+  margin-bottom: 15px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 8px;
+  margin-top: 5px;
+  box-sizing: border-box;
+`;
+
+const RequirementText = styled.p`
+  color: #555;
+  font-size: 12px;
+`;
+
+const StyledTextArea = styled.textarea`
+  width: 100%;
+  padding: 10px;
+  margin-top: 5px;
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  resize: vertical; /* Permite redimensionar verticalmente */
+`;
+
+const SelectInput = styled.select`
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-top: 5px;
 `;
 
 export default Reports;
