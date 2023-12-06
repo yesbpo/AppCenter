4fab17d9137cdea4f9e55d734eb44090b2929b4b
unknown <philiprockert@gmail.com>
2023-12-06 08:43:19 -0500

    limpieza user

--- pages/chats.js
+++ pages/chats.js
@@ -1,31 +1,31 @@
 import React, { useState, useEffect } from 'react'; 
 import Layout from '../components/Layout';
 import styled from 'styled-components';
-import axios from 'axios'; // Importa axios aquíimport io from 'socket.io-client';
-import MiComponente from '../components/websocket';
 import io from 'socket.io-client';
 const Chat = () => {
-  
+  const [chats, setChats] = useState([])
   const [mensajes, setMensajes] = useState({
     mensajeSaliente: [
-      {numeroDestino:'',tipo: '', contenido:''}],
+      {numeroDestino:'',tipo: '', contenido:'',date:''}],
     mensajesEntrantes: [
-      {numeroEntrante:'', tipo: '', contenido:'' }
+      {numeroEntrante:'', tipo: '', contenido:'',estado:'',date:''}
       ],
     inputValue: '',
   });
-  const [contactos, setContactos] = useState([{user: null, fecha:null}]);
+  
+  const [contactos, setContactos] = useState([{user: null, fecha:null, mensajes: [{tipomensaje:'',datemessage:'', content:''}]}]);
   const [webhookData, setWebhookData] = useState(null);
   const [num, setNum] =useState(null)  
+  const [numsaliente, setNumsaliente] = useState(null)
   useEffect(() => {
     // En tu aplicación de React
-const socket = io('https://3d29bmtd-8080.use2.devtunnels.ms/');
+    const socket = io('https://3d29bmtd-8080.use2.devtunnels.ms/');
 
 
     // Escuchar el evento 'cambio' y actualizar el estado del componente
-socket.on('cambio', data => {
-  console.log('Información del webhook recibida:', data);
-  const nuevosContactos = [...contactos,  {
+    socket.on('cambio', data => {
+    console.log('Información del webhook recibida:', data);
+    const nuevosContactos = [...contactos,  {
     
     user: data.payload.source,
     fecha: new Date(data.timestamp).toLocaleString('es-ES', {
@@ -38,51 +38,75 @@ socket.on('cambio', data => {
       timeZoneName: 'short',
     
   })}];
+  const nuevoMensajeEntrante = {
+    numeroEntrante: data.payload.source, // Ingresa el número de destino
+    tipo: data.type,
+    contenido: data.payload.payload.text,
+    date: data.timestamp
+  };
 
+  setMensajes((prevMensajes) => ({
+    ...prevMensajes,
+    mensajesEntrantes: [...prevMensajes.mensajesEntrantes, nuevoMensajeEntrante],
+     // Limpia el valor de entrada después de enviar
+  }));
   // Establece el estado de los contactos
   if (contactos.fecha !== nuevosContactos.fecha) {
     setContactos(nuevosContactos);
-  }
-;
+  };
   // Verificar si data.payload existe antes de acceder a data.payload.payload.text
   const webhookText = data ? data.payload.payload.text : null;
   const webhookNum = data ? data.payload.sender.phone : null;
+  const webhookNumsaliente = data ? data.payload.destination : null;
   // Llamar a setWebhookData con el texto del webhook solo si existe
   setWebhookData(webhookText);
-  setNum(webhookNum)
+  setNum(webhookNum);
+  setNumsaliente(webhookNumsaliente);
 });
-
-
-    // Limpiar el evento al desmontar el componente
+  // Limpiar el evento al desmontar el componente
     return () => {
       socket.disconnect();
     };
-  }, []);
-
-
-  const enviarMensaje = () => {
+    }, []);
+  // funciopn de llamado de api para envios simples
+  const enviarMensaje =  async () => {
     if (!mensajes.inputValue.trim()) {
       console.log('Mensaje vacío, no se enviará.');
       return;
     }
-
-    // Lógica para enviar el mensaje
-    const apiKey = '6ovjpik6ouhlyoalchzu4r2csmeqwlbg';
-
-    const data = {
-      message: {
-        type: 'text',
-        text: mensajes.inputValue,
-      },
+// Lógica para enviar el mensaje
+  const data = {
       channel: 'whatsapp',
-      'src.name': 'Pb1yes',
-      destination: '573124228889',
       source: '5718848135',
+      'src.name': 'Pb1yes',
+      destination: '573044575414',
+      message: mensajes.inputValue,
+      disablePreview: true,
     };
+   
+    try {
+      const response = await fetch('https://3d29bmtd-8080.use2.devtunnels.ms/api/envios', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams(data).toString(),
+      });
+      if (!response.ok) {
+        throw new Error(`Error en la solicitud: ${response.status} ${response.statusText}`);
+      }
+
+      const responseData = await response.json();
+      console.log('Respuesta del servidor:', responseData);
+      } catch (error) {
+      console.error('Error al realizar la solicitud:', error);
+      }
+    //nuevo mensaje saliente
     const nuevoMensaje = {
-      numeroDestino: '123456789', // Ingresa el número de destino
-      tipo: 'saliente',
+      numeroDestino: numsaliente, // Ingresa el número de destino
+      tipo: data.type,
       contenido: mensajes.inputValue,
+      estado: data.payload.type
     };
 
     setMensajes((prevMensajes) => ({
@@ -90,22 +114,6 @@ socket.on('cambio', data => {
       mensajeSaliente: [...prevMensajes.mensajeSaliente, nuevoMensaje],
       inputValue: '', // Limpia el valor de entrada después de enviar
     }));
-
-    axios.post('https://api.gupshup.io/sm/api/v1/msg', data, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'accept': 'application/json',
-        'apikey': apiKey,
-      },
-    })
-    .then(response => {
-      // Manejar la respuesta aquí
-      console.log(response.data);
-    })
-    .catch(error => {
-      // Manejar errores aquí
-      console.error('Error:', error);
-    });
   };
 // usuarios
 useEffect(() => {
@@ -130,6 +138,7 @@ useEffect(() => {
     }));
 
     // Establece el estado de los contactos
+
     setContactos(nuevosContactos);
   
       
@@ -140,7 +149,6 @@ useEffect(() => {
   }, []);   
 return (
     <Layout>
-      
       <Box>
         <ButtonContainer>
           <CustomButton onClick={() => console.log('Activos')}>Activos</CustomButton>
@@ -149,47 +157,42 @@ return (
           <CustomButton onClick={() => console.log('Agregar Número')}>Agregar Número</CustomButton>
         </ButtonContainer>
       </Box>
-
       <Container>
         <Box>
          
-          <div className="chat-container">
-        
-      <p> Numero: {num} mensaje:{webhookData}</p>
-      {/* Mostrar mensajes entrantes */}
-      {mensajes.mensajesEntrantes.map((mensaje, index) => (
-        <div key={index} className="mensaje entrante">
+        <div className="chat-container">
+          <p> Numero: {num} mensaje:{webhookData}</p>
+          {/* Mostrar mensajes entrantes */}
+          {mensajes.mensajesEntrantes.map((mensaje, index) => (
+          <div key={index} className="mensaje entrante">
           {mensaje.contenido}
         </div>
-      ))}
+        ))}
 
-      {/* Mostrar mensajes salientes */}
-      {mensajes.mensajeSaliente.map((mensaje, index) => (
+        {/* Mostrar mensajes salientes */}
+        {mensajes.mensajeSaliente.map((mensaje, index) => (
         <div key={index} className="mensaje saliente">
           {mensaje.contenido}
         </div>
-      ))}
+        ))}
 
-      {/* Input para enviar mensajes */}
+        {/* Input para enviar mensajes */}
       
-    </div>
+      </div>
           
         </Box>
-
         <Box>
           <div className="chat-container">
             <ul>
             {contactos.map((contacto, index) => (
-          <li key={index}>
-            <strong>Usuario:</strong> {contacto.user}, <strong>Fecha:</strong> {contacto.fecha}
-          </li>
-        ))}
+              <li key={index}>
+                <strong onClick={<div>{contacto.user}</div>}>Usuario:</strong> {contacto.user}, <strong>Fecha:</strong> {contacto.fecha}
+              </li>
+              ))}
             </ul>
-            
           </div>
         </Box>
       </Container>
-
       <Box>
         <div className="input-container">
           <input
--- pages/users.js
+++ pages/users.js
@@ -13,134 +13,14 @@ import {
     selectUserUsername,
     setCurrentUser,
 } from "../store/userSlice";
-import { firestore } from "../utils/firebase";
+    import { firestore } from "../utils/firebase";
 
-// User Table Component
-const Users = (props) => {
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [selectedUser, setSelectedUser] = useState(null);
-    const [pageCount, setPageCount] = useState(0);
-    const pageIndexRef = useRef(0);
-    const lastUserVisibleSnapshot = useRef(null);
-    const firstUserVisibleSnapshot = useRef(null);
-    const fetchIdRef = useRef(0);
-    const userName = useSelector(selectUserUsername);
-    const authState = useSelector(selectAuthState);
-    const dispatch = useDispatch();
-    const currentUserLogged = useSelector(selectCurrentUser, shallowEqual);
-    const router = useRouter();
-
-    const fetchUsers = useCallback(
-        (pageIndex, pageSize) => {
-            const fetchId = ++fetchIdRef.current;
-            setLoading(true);
-            // Only update the data if this is the latest fetch
-            if (fetchId === fetchIdRef.current) {
-                const isFoward = pageIndexRef.current <= pageIndex
-                getUsers(isFoward, pageSize);
-                pageIndexRef.current = pageIndex;
-            }
-        }, [],
-    );
-
-
-    const getUsers = async(isForward, pageSize) => {
-        // If we already have a last user visible snapshot it means that will be the first doc for the next page.
-        if (lastUserVisibleSnapshot.current !== null) {
-            firstUserVisibleSnapshot.current = lastUserVisibleSnapshot.current;
-        }
-
-        // Collection.
-        const docCollection = collection(firestore, "users");
-        // Query reference.
-        let qRef;
-
-        if (isForward) {
-            lastUserVisibleSnapshot.current === null ?
-                qRef = query(
-                    docCollection,
-                    orderBy("userName"),
-                    limit(pageSize)
-                ) :
-                qRef = query(
-                    docCollection,
-                    orderBy("userName"),
-                    startAfter(lastUserVisibleSnapshot.current),
-                    limit(pageSize)
-                );
-        } else {
-            firstUserVisibleSnapshot.current === null ?
-                qRef = query(
-                    docCollection,
-                    orderBy("userName"),
-                    limit(pageSize)
-                ) :
-                qRef = query(
-                    docCollection,
-                    orderBy("userName"),
-                    endBefore(lastUserVisibleSnapshot.current),
-                    limit(pageSize)
-                );
-        }
-
-        // Getting results for query.
-        const querySnapshot = await getDocs(qRef);
-        setUsers([]);
-        let userArray = [];
-        querySnapshot.forEach((doc) => {
-            const uid = doc.id;
-            const { email, userName, role, phone, name, bloodType } = doc.data();
-            userArray.push({
-                uid,
-                email,
-                userName,
-                role,
-                phone,
-                name,
-                bloodType
-            })
-        });
-        console.log("userarray" + userArray);
-        setUsers(userArray)
-
-        // Save reference to the last & first visible users to be able to paginate.
-        const snapshotLastVisibleUser = querySnapshot.docs[querySnapshot.docs.length - 1];
-        lastUserVisibleSnapshot.current = snapshotLastVisibleUser;
-
-        // Get page count.
-        if (pageCount === 0) {
-            const snapshot = await getCountFromServer(collection(firestore, "users"));
-            setPageCount(Math.ceil(snapshot.data().count / pageSize))
-        }
-
-        if (currentUserLogged.role === null || currentUserLogged.role === "") {
-            getUserData(userArray);
-        }
-        setLoading(false)
-    }
-
-    const getUserData = async (usersArray) => {
-        const currentUser = usersArray.find((user) => user.email === userName);
-        if (currentUser !== undefined) {
-            dispatch(setCurrentUser(currentUser));
-        } else {
-            // If we don't get it from the usersArray then we can search for it using the username.
-            // Remember the userName is the id + @yesbpo.com that's why we split it.
-            const docRef = doc(firestore, "users", userName.split("@")[0]);
-            const docSnap = await getDoc(docRef);
-
-            if (docSnap.exists()) {
-                console.log("Document data:", docSnap.data());
-                // Save the data as the currentUset.
-                // TODO: - dispatch(setCurrentUser(docSnap.data()))
-            } else {
-                // doc.data() will be undefined in this case
-                console.log("No such document!");
-            }
-        }
-    };
-
+    // User Table Component
+    const Users = (props) => {
+        const [users, setUsers] = useState([]);
+        ;
+        const authState = useSelector(selectAuthState);
+       
     const verifyUserAuthState = () => {
         if (!authState) {
             router.push("/api/auth/signin");
@@ -151,69 +31,11 @@ const Users = (props) => {
         verifyUserAuthState();
     }, [currentUserLogged]);
 
-    const columns = React.useMemo(
-        () => [{
-                Header: "Nombre de usuario",
-                accessor: "userName"
-            },
-            {
-                Header: "Nombre",
-                accessor: "name",
-            },
-            {
-                Header: "Rol",
-                accessor: "role",
-            }
-        ], []
-    );
-
-    const [showModal, setShowModal] = useState(false);
-    const data = React.useMemo(() => users, [users]);
-
-    const modalHandler = () => {
-        setSelectedUser(null);
-        setShowModal(!showModal);
-    };
-
-    const modalhandlerEdit = (row) => {
-        console.log("user Uid" + row.uid);
-        setSelectedUser(row)
-        setShowModal(!showModal)
-    }
+    
+    
     return (
         <Layout>
-            <div className="App" style={{ height: "100%" }}>
-                <div className="min-h-screen bg-light text-gray-900" >
-                    <main className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 pt-4" >
-                        <div className="mt-4" > 
-                        {
-                            currentUserLogged.role === "Coordinador" &&
-                                <div className="flex flex-row max-h-12 justify-between mb-4" >
-                                    <UIButton onClick={modalHandler}
-                                        className="bg-green w-36 text-light-lighter hover:bg-[#9ec998]" > CREAR </UIButton>
-                                </div>
-                        }
-                            <Table
-                                columns={columns}
-                                data={data}
-                                fetchData={fetchUsers}
-                                controlledPageCount={pageCount}
-                                loading={loading}
-                                handleTapOnRow={modalhandlerEdit} />
-                            {
-                                showModal &&
-                                <div
-                                    className="fixed inset-0 flex items-center justify-center bg-text bg-opacity-50 backdrop-blur-sm" >
-                                    <CreateUserModal
-                                        closeModal={modalHandler}
-                                        editUser={selectedUser}
-                                    />
-                                </div>
-                            }
-                        </div>
-                    </main>
-                </div>
-            </div>
+           
         </Layout>
     );
 };
