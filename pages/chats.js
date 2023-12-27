6562d1f48a62ec807602bcc50910d5eb70ced508
unknown <philiprockert@gmail.com>
2023-12-27 14:11:10 -0500

    cambios para produccion1

--- pages/chats.js
+++ pages/chats.js
@@ -26,7 +26,7 @@ const Chats = () => {
   const toggleEmojiPicker = () => {
     setShowEmojiPicker((prevShow) => !prevShow);
   };
-  const socket = io('http://45.55.129.170:8080/');
+  const socket = io('https://appcenteryes.appcenteryes.com/w/');
   const [contactos1, setContactos1] = useState([]);
   const [contactos, setContactos] = useState([
     { user: null, fecha: null, mensajes: [{ tipomensaje: '', datemessage: '', content: '' }] },
@@ -36,9 +36,9 @@ const Chats = () => {
   
      const handlePendientesClick = async () => {
     try {
-      const response = await fetch('https://146.190.143.165:3001/obtener-mensajes');
-      const responseChats = await fetch('https://146.190.143.165:3001/obtener-chats');
-      const responseUsers = await fetch('https://146.190.143.165:3001/obtener-usuarios');
+      const response = await fetch('https://appcenteryes.appcenteryes.com/db/obtener-mensajes');
+      const responseChats = await fetch('https://appcenteryes.appcenteryes.com/db/obtener-chats');
+      const responseUsers = await fetch('https://appcenteryes.appcenteryes.com/db/obtener-usuarios');
       // El usuario está autenticado, puedes acceder a la sesión
       
       if (!response.ok) {
@@ -60,9 +60,9 @@ const Chats = () => {
   };
   const handleEngestionClick = async () => {
     try {
-      const response = await fetch('https://146.190.143.165:3001/obtener-mensajes');
-      const responseChats = await fetch('https://146.190.143.165:3001/obtener-chats');
-      const responseUsers = await fetch('https://146.190.143.165:3001/obtener-usuarios');
+      const response = await fetch('https://appcenteryes.appcenteryes.com/db/obtener-mensajes');
+      const responseChats = await fetch('https://appcenteryes.appcenteryes.com/db/obtener-chats');
+      const responseUsers = await fetch('https://appcenteryes.appcenteryes.com/db/obtener-usuarios');
       // El usuario está autenticado, puedes acceder a la sesión
       
       if (!response.ok) {
@@ -129,7 +129,7 @@ const [url, setUrl] = useState('');
           disablePreview: true,
         };
 
-        const responseEnvio = await fetch('https://3d29bmtd-8080.use2.devtunnels.ms/api/envios', {
+        const responseEnvio = await fetch('https://appcenteryes.appcenteryes.com/w/api/envios', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
@@ -146,7 +146,7 @@ const [url, setUrl] = useState('');
 
         const idMessage = responseData.messageId;
 
-        const actualizarMensajeResponse = await fetch('https://146.190.143.165:3001/mensajeenviado', {
+        const actualizarMensajeResponse = await fetch('https://appcenteryes.appcenteryes.com/db/mensajeenviado', {
           method: 'PUT',
           headers: {
             'Content-Type': 'application/json',
@@ -180,7 +180,7 @@ const [mostrarPendientes, setMostrarPendientes] = useState(false);
       const idChat2 = numeroEspecifico; // Asegúrate de obtener el idChat2 según tu lógica
       const nuevoEstado = 'in process'; // Asegúrate de obtener el nuevoEstado según tu lógica
 
-      const response = await fetch('https://146.190.143.165:3001/actualizar-estado-chat', {
+      const response = await fetch('https://appcenteryes.appcenteryes.com/db/actualizar-estado-chat', {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
@@ -221,7 +221,7 @@ const [mostrarPendientes, setMostrarPendientes] = useState(false);
     console.log('Información del webhook recibida:', data);
     
         try {
-          const response = await fetch('https://146.190.143.165:3001/obtener-mensajes');
+          const response = await fetch('https://appcenteryes.appcenteryes.com/db/obtener-mensajes');
   
           if (!response.ok) {
             throw new Error(`Error en la solicitud: ${response.status} ${response.statusText}`);
@@ -340,7 +340,7 @@ const [mostrarPendientes, setMostrarPendientes] = useState(false);
       const idMessage = responseData.messageId;
 
       // Actualiza el mensaje en el servidor
-      const actualizarMensajeResponse = await fetch('https://146.190.143.165:3001/mensajeenviado', {
+      const actualizarMensajeResponse = await fetch('https://appcenteryes.appcenteryes.com/db/mensajeenviado', {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
@@ -357,7 +357,7 @@ const [mostrarPendientes, setMostrarPendientes] = useState(false);
       } else {
         console.error('Error al actualizar el mensaje:', actualizarMensajeResponse.status);
           // Guarda el mensaje en el servidor
-    const guardarMensajeResponse = await fetch('https://146.190.143.165:3001/guardar-mensajes', {
+    const guardarMensajeResponse = await fetch('https://appcenteryes.appcenteryes.com/db/guardar-mensajes', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -436,7 +436,7 @@ const [mostrarPendientes, setMostrarPendientes] = useState(false);
         console.error('Error al actualizar el usuario:', response.statusText);
       }
       try {
-        const response = await fetch('https://146.190.143.165:3001/obtener-mensajes');
+        const response = await fetch('https://appcenteryes.appcenteryes.com/db/obtener-mensajes');
 
         if (!response.ok) {
           throw new Error(`Error en la solicitud: ${response.status} ${response.statusText}`);
