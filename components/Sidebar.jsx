cfad12ab0ccf6445576b3e478f6d23a9275c8c2a
unknown <philiprockert@gmail.com>
2023-12-29 11:58:14 -0500

    subida al servidor

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -68,7 +68,7 @@ const Sidebar = (props) => {
     const nuevoDato = 'Inactivo'; // Reemplaza con el nuevo valor que deseas asignar
   
     try {
-      const response = await fetch('https://appcneteryes.appcenteryes.com/db/actualizar/usuario', {
+      const response = await fetch('https://appcenteryes.appcenteryes.com/db/actualizar/usuario', {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
