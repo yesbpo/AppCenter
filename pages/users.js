77595696a480116b1d3a2f808ecbb5fae0c0fcc5
unknown <philiprockert@gmail.com>
2023-12-30 10:57:48 -0500

    subida al servidor

--- pages/users.js
+++ pages/users.js
@@ -44,39 +44,42 @@ const CrearUsuario = () => {
   return (
     
     <Layout>
-       <div className="container mt-10">
-       <h1 className="text-dark">Crear Usuario</h1>
-        <form>
-          <div className="mb-5">
-            <label htmlFor="usuario" className="form-label">Usuario:</label>
-            <input type="text" className="form-control" id="usuario" value={usuario} onChange={(e) => setUsuario(e.target.value)} />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">Password:</label>
-            <input type="password" className="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">Email:</label>
-            <input type="email" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="session" className="form-label">Session:</label>
-            <input type="text" className="form-control" id="session" value={session} onChange={(e) => setSession(e.target.value)} />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="typeUser" className="form-label">Type User:</label>
-            <input type="text" className="form-control" id="typeUser" value={typeUser} onChange={(e) => setTypeUser(e.target.value)} />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="completeName" className="form-label">Complete Name:</label>
-            <input type="text" className="form-control" id="completeName" value={complete_name} onChange={(e) => setComplete_name(e.target.value)} />
-          </div>
-          <button type="button" className="btn btn-dark" onClick={handleCrearUsuario}>
-            Crear Usuario
-          </button>
-        </form>
-      </div>
-    </Layout>
+  <div className="flex items-center justify-center h-screen">
+    <div className="container">
+      <h1 className="text-dark text-center mb-6">Crear Usuario</h1>
+      <form>
+        <div className="mb-5">
+          <label htmlFor="usuario" className="form-label">Usuario:</label>
+          <input type="text" className="form-control" id="usuario" value={usuario} onChange={(e) => setUsuario(e.target.value)} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password:</label>
+          <input type="password" className="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email:</label>
+          <input type="email" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="session" className="form-label">Session:</label>
+          <input type="text" className="form-control" id="session" value={session} onChange={(e) => setSession(e.target.value)} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="typeUser" className="form-label">Type User:</label>
+          <input type="text" className="form-control" id="typeUser" value={typeUser} onChange={(e) => setTypeUser(e.target.value)} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="completeName" className="form-label">Complete Name:</label>
+          <input type="text" className="form-control" id="completeName" value={complete_name} onChange={(e) => setComplete_name(e.target.value)} />
+        </div>
+        <button type="button" className="btn btn-dark" onClick={handleCrearUsuario}>
+          Crear Usuario
+        </button>
+      </form>
+    </div>
+  </div>
+</Layout>
+
       )}
       return (
         <>
