b794309c76ff1ab2ee7f3cde2b563c91e4328885
unknown <philiprockert@gmail.com>
2023-12-30 11:03:34 -0500

    subida al servidor

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,8 @@ import { persister, store } from "../store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import {Provider} from "react-redux";
 import { SessionProvider } from "next-auth/react"
+import '../styles/sidebar.css'
 import '../styles/templates.css'
-
 import '../styles/envios.css'
 
 function MyApp({Component, pageProps}) {
