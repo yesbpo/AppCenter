72c2283388779119eeeea6a7052d2c5493f63bb7
Felipe Rodriguez Sarmiento <mesadeayuda@yesbpo.co>
2024-01-01 17:25:29 -0500

    v1

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,9 +5,9 @@ const Layout = ({children}) => {
   const { data: session } = useSession()
   if (session) {
     return (
-        <div className="h-full flex flex-row justify-start">
+        <div className="h-screen flex">
            <Sidebar/>
-           <div className="flex-1">
+           <div className="flex-1 overflow-y-auto">
              {children}
            </div>
         </div>
