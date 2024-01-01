3989bfe02ddf973979f0c983bf6a7550d3cffabd
Felipe Rodriguez Sarmiento <mesadeayuda@yesbpo.co>
2024-01-01 16:07:54 -0500

    v1

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -39,7 +39,7 @@ function LoginPage() {
           <p className="bg-red-500 text-lg text-white p-3 rounded mb-2">{error}</p>
         )}
 
-        <h1 className="text-slate-200 font-bold text-4xl mb-4">Login</h1>
+        
         <img className="p-3 rounded block mb-2 bg-slate-900 text-slate-300 mx-auto my-auto" src="https://1bb437.a2cdn1.secureserver.net/wp-content/uploads/2023/08/Logo-500-full-150x150.jpg" alt="Logo" />
 
         <label htmlFor="email" className="text-slate-500 mb-2 block text-sm">
